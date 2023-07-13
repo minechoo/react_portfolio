1d912fdb607e40a03d7338cfd271e18b51491f06
minechoo <choominhae@gamil.com>
2023-07-13 16:12:11 +0900

    유트브 갯수

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
 	const fetchYoutube = async () => {
 		const key = 'AIzaSyANMdnk7q2cBX8tqGJZXpVFH9bGJMOwmEc'; //api 키
 		const list = 'PLMafzyXZ12TPBYgeplFEdJeSMcJvb3v5u'; //class 브라우저 상단값
-		const num = 10;
+		const num = 8;
 		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${list}&key=${key}&maxResults=${num}`;
 
 		const result = await axios.get(url);
