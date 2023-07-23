30177a0fcb0d7a2203fa86cc0ff4dc926385773a
minechoo <choominhae@gamil.com>
2023-07-23 09:42:29 +0900

    main 상단

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -1,9 +1,20 @@
 import { memo } from 'react';
 function Visual() {
 	return (
-		<figure id='visual' className='myScroll'>
-			<video src={process.env.PUBLIC_URL + '/img/vid.mp4'} loop autoPlay muted></video>
-		</figure>
+		<section id='wrap_first' className='myScroll'>
+			{/* <div className='video_wrap'>
+				<video src={process.env.PUBLIC_URL + '/img/vid.mp4'} loop autoPlay muted></video>
+			</div> */}
+
+			<div className='txt_main'>
+				<h2>Join our team</h2>
+				<p>Work and be tourself !</p>
+				<div className='txt_round'>
+					<p>Open Positions</p>
+					<span>4</span>
+				</div>
+			</div>
+		</section>
 	);
 }
 export default memo(Visual);
